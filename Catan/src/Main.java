2fb9412d3a101b45a663902b0e427de7b0d46e83
GiancarloMassimo <gmassimo2004@gmail.com>
2022-03-11 17:01:09 -0600

    Images

--- Catan/src/Main.java
+++ Catan/src/Main.java
@@ -1,7 +1,8 @@
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        int x = 5;
-        System.out.println(x);
-        System.out.println("This is Giancarlo's branch");
+        ImageLoader.loadImages();
+        Frame frame = new Frame("Settlers of Catan");
     }
 }

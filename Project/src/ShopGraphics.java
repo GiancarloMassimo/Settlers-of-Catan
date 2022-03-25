c23b790708951c4cf1855575715cbf602bb22bf2
Sulaiman Mulla <sumulla@hotmail.com>
2022-03-24 19:17:12 -0500

    added inventory icons

--- Project/src/ShopGraphics.java
+++ Project/src/ShopGraphics.java
@@ -3,17 +3,28 @@ import java.awt.image.BufferedImage;
 
 public class ShopGraphics implements GraphicsItem
 {
-    private BufferedImage inventoryText, shopText;
+    private BufferedImage inventoryText, shopText, woodpic, brickpic, wheatpic, sheeppic, orepic;
 
     public ShopGraphics()
     {
         inventoryText = ImageLoader.getImage("Inventory_text");
         shopText = ImageLoader.getImage("Shop_");
+        woodpic = ImageLoader.getImage("woodpic");
+        brickpic = ImageLoader.getImage("brickpic");
+        wheatpic = ImageLoader.getImage("wheatpic");
+        sheeppic = ImageLoader.getImage("sheeppic");
+        orepic = ImageLoader.getImage("orepic");
+
     }
     public void draw(Graphics g)
     {
         g.drawImage(inventoryText, 39, 747, 188, 44, null);
         g.drawImage(shopText, 39, 822, 103, 46, null);
+        g.drawImage(woodpic, 245, 732, 83, 88, null);
+        g.drawImage(brickpic, 340, 732, 83, 88, null);
+        g.drawImage(wheatpic, 435, 732, 83, 88, null);
+        g.drawImage(sheeppic, 530, 732, 83, 88, null);
+        g.drawImage(orepic, 625, 732, 83, 88, null);
     }
 
 }

200e667179142ed9d2bd8962afe801f14829f0df
Sulaiman Mulla <sumulla@hotmail.com>
2022-04-07 13:25:06 -0500

    4/7/2022: I added the VP indicator. I am commiting and pushing because I am about to pull some data from the main branch

--- Project/src/PlayerGraphics.java
+++ Project/src/PlayerGraphics.java
@@ -3,16 +3,21 @@ import java.awt.image.BufferedImage;
 
 public class PlayerGraphics implements GraphicsItem
 {
-    private BufferedImage RedPlayer, YellowPlayer, BluePlayer, GreenPlayer;
+    private BufferedImage RedPlayer, YellowPlayer, BluePlayer, GreenPlayer, VPindicator;
     private BufferedImage NormalRoad, LongestRoad, NormalArmy, LargestArmy;
 
     public PlayerGraphics()
     {
+        //players
         RedPlayer = ImageLoader.getImage("Red Player");
         YellowPlayer = ImageLoader.getImage("Yellow Player");
         BluePlayer = ImageLoader.getImage("Blue Player");
         GreenPlayer = ImageLoader.getImage("Green Player");
 
+        //other
+        VPindicator = ImageLoader.getImage("Victory Point Indicator");
+
+        //roads and armies
         NormalRoad = ImageLoader.getImage("Normal Road");
         LongestRoad = ImageLoader.getImage("Longest Road");
         NormalArmy = ImageLoader.getImage("Normal Army");
@@ -20,22 +25,27 @@ public class PlayerGraphics implements GraphicsItem
     }
     public void draw(Graphics g)
     {
+        //Red's player components
         g.drawImage(RedPlayer, 1040, 300, 544 , 81 , null);
-        g.drawImage(YellowPlayer, 1040, 400, 544 , 81 , null);
-        g.drawImage(BluePlayer, 1040, 500, 544 , 81 , null);
-        g.drawImage(GreenPlayer, 1040, 600, 544 , 81 , null);
-
-        g.drawImage(NormalRoad, 1140, 320, 77 , 26 , null); //Red's player components
+        g.drawImage(NormalRoad, 1140, 320, 77 , 26 , null);
         g.drawImage(NormalArmy, 1255, 315, 15 , 40 , null);
 
-        g.drawImage(NormalRoad, 1140, 420, 77 , 26 , null);//Yellow's player components
+        //Yellow's player components
+        g.drawImage(YellowPlayer, 1040, 400, 544 , 81 , null);
+        g.drawImage(NormalRoad, 1140, 420, 77 , 26 , null);
         g.drawImage(NormalArmy, 1255, 415, 15 , 40 , null);
 
-        g.drawImage(NormalRoad, 1140, 520, 77 , 26 , null);//Blue's player components
+        //Blue's player components
+        g.drawImage(BluePlayer, 1040, 500, 544 , 81 , null);
+        g.drawImage(NormalRoad, 1140, 520, 77 , 26 , null);
         g.drawImage(NormalArmy, 1255, 515, 15 , 40 , null);
 
-        g.drawImage(NormalRoad, 1140, 620, 77 , 26 , null);// Green's player components
+        // Green's player components
+        g.drawImage(GreenPlayer, 1040, 600, 544 , 81 , null);
+        g.drawImage(NormalRoad, 1140, 620, 77 , 26 , null);
         g.drawImage(NormalArmy, 1255, 615, 15 , 40 , null);
+
+        //g.drawImage(VPindicator,1090,295,30,30,null);
 
     }
 }

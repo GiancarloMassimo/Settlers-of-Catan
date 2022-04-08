f9e8df5431dac8d9b12fcb488fe0d848c71d420b
GiancarloMassimo <gmassimo2004@gmail.com>
2022-04-07 21:40:29 -0500

    Create graph structure

--- Project/src/Map.java
+++ Project/src/Map.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 
 public class Map {
     private Tile[][] map;
-
+    private Node[] nodes;
 
     public Map() {
         createMap();
@@ -14,9 +14,22 @@ public class Map {
         TileMapFactory tileMapFactory = new TileMapFactory();
         map = Arrays.copyOf(tileMapFactory.getMap(), tileMapFactory.getMap().length);
         tileMapFactory = null;
+
+        NodeGraphFactory nodeGraphFactory = new NodeGraphFactory(map);
+        nodes = Arrays.copyOf(nodeGraphFactory.getNodes(), nodeGraphFactory.getNodes().length);
+
+        for (int i = 1; i <= nodes.length; i++) {
+            System.out.println(i + ". "
+                    + (nodes[i - 1] == null ? "null" : nodes[i - 1].getAdjacentTiles().toString()) + " "
+                    + nodes[i - 1].getEdges().size());
+        }
     }
 
     public Tile[][] getMap() {
         return map;
+    }
+
+    public Node[] getNodes() {
+        return nodes;
     }
 }
--- Project/src/PlayerGraphics.java
+++ Project/src/PlayerGraphics.java
@@ -25,25 +25,27 @@ public class PlayerGraphics implements GraphicsItem
     }
     public void draw(Graphics g)
     {
+        //TODO: WRITE THIS WITH A FOR LOOP USING PLAYER DATA
+        //The way this is right now is going to be really tedious
         //Red's player components
-        g.drawImage(RedPlayer, 1040, 300, 544 , 81 , null);
-        g.drawImage(NormalRoad, 1140, 320, 77 , 26 , null);
-        g.drawImage(NormalArmy, 1255, 315, 15 , 40 , null);
+        g.drawImage(RedPlayer, 1050, 300, 544 , 81 , null);
+        g.drawImage(NormalRoad, 1150, 320, 77 , 26 , null);
+        g.drawImage(NormalArmy, 1265, 315, 15 , 40 , null);
 
         //Yellow's player components
-        g.drawImage(YellowPlayer, 1040, 400, 544 , 81 , null);
-        g.drawImage(NormalRoad, 1140, 420, 77 , 26 , null);
-        g.drawImage(NormalArmy, 1255, 415, 15 , 40 , null);
+        g.drawImage(YellowPlayer, 1050, 400, 544 , 81 , null);
+        g.drawImage(NormalRoad, 1150, 420, 77 , 26 , null);
+        g.drawImage(NormalArmy, 1265, 415, 15 , 40 , null);
 
         //Blue's player components
-        g.drawImage(BluePlayer, 1040, 500, 544 , 81 , null);
-        g.drawImage(NormalRoad, 1140, 520, 77 , 26 , null);
-        g.drawImage(NormalArmy, 1255, 515, 15 , 40 , null);
+        g.drawImage(BluePlayer, 1050, 500, 544 , 81 , null);
+        g.drawImage(NormalRoad, 1150, 520, 77 , 26 , null);
+        g.drawImage(NormalArmy, 1265, 515, 15 , 40 , null);
 
         // Green's player components
-        g.drawImage(GreenPlayer, 1040, 600, 544 , 81 , null);
-        g.drawImage(NormalRoad, 1140, 620, 77 , 26 , null);
-        g.drawImage(NormalArmy, 1255, 615, 15 , 40 , null);
+        g.drawImage(GreenPlayer, 1050, 600, 544 , 81 , null);
+        g.drawImage(NormalRoad, 1150, 620, 77 , 26 , null);
+        g.drawImage(NormalArmy, 1265, 615, 15 , 40 , null);
 
         //g.drawImage(VPindicator,1090,295,30,30,null);
 

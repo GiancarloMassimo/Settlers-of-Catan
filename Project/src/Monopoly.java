852ba865e429f17ba2cdafc6d9ddc5e6bd1e0e1b
Hedroja <johuenter@gmail.com>
2022-04-12 13:00:03 -0500

    Monopoly
    VictoryPointCard
    
    Year of plenty

--- Project/src/Monopoly.java
+++ Project/src/Monopoly.java
@@ -1,10 +1,25 @@
-public class Monopoly extends ProgressCard{
-
-    public Monopoly(Player o){
-        super(o,ProgressCardType.Monopoly);
-    }
-
-//    public void use(String req){ get one requested card from all players
+//public class Monopoly extends ProgressCard{
 //
+//    public Monopoly(Player o){
+//        super(o,ProgressCardType.Monopoly);
 //    }
-}
+//    //get one requested card from all players, once finished returns the updated player arraylist to update all card counts
+//    public Player[] use(String req, Player[] playerList){
+//        Player[] pl = playerList;
+//        int cnt=0;
+//        Inventory currentPlayerInventory;
+//        Player owner = super.getOwner();
+//        //subtracts 1 from the number of that players resource excluding the owner/user, then counts the amount the owner gets
+//        for (int i =0;i< playerList.length;i++) {
+//            currentPlayerInventory=playerList[i].getPlayerInventory();
+//            if (playerList[i] != owner) {
+//                if (currentPlayerInventory.getResourceCnt(req) != 0)
+//                    currentPlayerInventory.changeResource(req,-1);
+//                cnt++;
+//            }
+//        }
+//
+//
+//        return pl;
+//    }
+//}
--- Project/src/YearofPlenty.java
+++ Project/src/YearofPlenty.java
@@ -5,34 +5,32 @@ public class YearofPlenty extends ProgressCard{
     }
 
     public void use(String c1, String c2){  //gives two of any resource of choice
-        String choice1 = c1;
-        String choice2 = c2;
-        switch(choice1)
-        {
-            case "brick":
-                super.getOwner().getPlayerInventory().changeBrick(1);
-            case "sheep":
-                super.getOwner().getPlayerInventory().changeSheep(1);
-            case "ore":
-                super.getOwner().getPlayerInventory().changeOre(1);
-            case "wheat":
-                super.getOwner().getPlayerInventory().changeWheat(1);
-            case "wood":
-                super.getOwner().getPlayerInventory().changeWood(1);
-        }
-        switch(choice2)
-        {
-            case "brick":
-                super.getOwner().getPlayerInventory().changeBrick(1);
-            case "sheep":
-                super.getOwner().getPlayerInventory().changeSheep(1);
-            case "ore":
-                super.getOwner().getPlayerInventory().changeOre(1);
-            case "wheat":
-                super.getOwner().getPlayerInventory().changeWheat(1);
-            case "wood":
-                super.getOwner().getPlayerInventory().changeWood(1);
-        }
+//        switch(c1)
+//        {
+//            case "brick":
+//                super.getOwner().getPlayerInventory().changeBrick(1);
+//            case "sheep":
+//                super.getOwner().getPlayerInventory().changeSheep(1);
+//            case "ore":
+//                super.getOwner().getPlayerInventory().changeOre(1);
+//            case "wheat":
+//                super.getOwner().getPlayerInventory().changeWheat(1);
+//            case "wood":
+//                super.getOwner().getPlayerInventory().changeWood(1);
+//        }
+//        switch(c2)
+//        {
+//            case "brick":
+//                super.getOwner().getPlayerInventory().changeBrick(1);
+//            case "sheep":
+//                super.getOwner().getPlayerInventory().changeSheep(1);
+//            case "ore":
+//                super.getOwner().getPlayerInventory().changeOre(1);
+//            case "wheat":
+//                super.getOwner().getPlayerInventory().changeWheat(1);
+//            case "wood":
+//                super.getOwner().getPlayerInventory().changeWood(1);
+//        }
 
     }
 }

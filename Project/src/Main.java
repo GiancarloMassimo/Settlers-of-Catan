7a6e171168d927858273572e3f4e617c0a6268b4
GiancarloMassimo <gmassimo2004@gmail.com>
2022-04-12 23:13:28 -0500

    Commit unversioned files

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,9 +1,6 @@
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
         GameLog log = new GameLog();
-
         GameManager gameManager = new GameManager();
         Frame frame = new Frame("Settlers of Catan: Team MatthewZ");
     }

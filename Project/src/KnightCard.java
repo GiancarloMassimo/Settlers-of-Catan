c248f74eba55630372f4818b1a9be9214e1daaa3
Hedroja <90929032+Hedroja@users.noreply.github.com>
2022-04-21 13:34:36 -0500

    Knight

--- Project/src/KnightCard.java
+++ Project/src/KnightCard.java
@@ -1,13 +1,11 @@
-import java.util.ArrayList;
-
 public class KnightCard extends DevelopmentCard{
 
     public KnightCard(Player o) {
         super(o);
     }
 
-//    public ArrayList<Player> use(int c, int r, Robber rob, Tile[][] map){
-//
-//    }
+    public void use(){
+        ItemPlacementController.placeRobber();
+    }
 
 }
